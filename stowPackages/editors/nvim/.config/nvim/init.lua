572623c4c8456115be8922d1eef54e561eb1df2c
rumi <mailark@cock.li>
2026-03-22 16:14:31 +0530

    (nvim) add new colorscheme and lualine

--- stowPackages/editors/nvim/.config/nvim/init.lua
+++ stowPackages/editors/nvim/.config/nvim/init.lua
@@ -11,7 +11,9 @@ vim.cmd.colo("retrobox")
 
 
 vim.pack.add({
-    "https://github.com/nvim-treesitter/nvim-treesitter"
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+    "https://github.com/ellisonleao/gruvbox.nvim",
+    "https://github.com/nvim-lualine/lualine.nvim"
 })
 require("nvim-treesitter").setup()
 require("nvim-treesitter").install({"python","lua"})
@@ -20,3 +22,6 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'python','lua' },
   callback = function() vim.treesitter.start() end,
 })
+
+vim.cmd.colorscheme("gruvbox")
+require("lualine").setup()
